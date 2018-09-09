deb7052a454f70e334e8b90dbfdb41ff1ec5afdb
John Howells <jhowells@sugarcrm.com>
2018-09-09 10:18:44 -0700

    Update getImage.php

--- getImage.php
+++ getImage.php
@@ -1,4 +1,10 @@
+
 <?php
+
+    // This is the client portion of the ProfileImageFinder project. It's just PHP and HTML calling itself after
+    // an email address has been passed in through the form. Some data sanitizing is being done.
+
+    session_start();
 
     include_once('Services/ImageService.php');
 
@@ -7,30 +13,32 @@
 ?>
 
     <form method = "POST" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <table>
+    <table align=center>
         <tr>
             <td>Email Address:</td>
             <td><input type = "text" name = "email_address">
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <input type = "submit" name = "submit" value = "Submit">
-            </td>
+            </td><td><input type = "submit" name = "submit" value = "Submit"></td>
         </tr>
     </table>
     </form>
 
 <?php
 
-    $lookup = new ImageService();
-
-    list($result, $fullName) = $lookup->lookupByEmail($email_address);
-    if (!$result) {
-        echo "We were unable to find any photos to display for $email_address. Please verify the email address and resubmit.<br>";
+    // check session data to see if we've already done this lookup
+    if (isset($_SESSION[$email_address])) {
+        $photo_url = $_SESSION[$email_address];
     } else {
-        echo "<table align=center><tr><td align=center><img src=$result></td></tr></table>";
-        echo "<table align=center><tr><td align=center>$fullName</td></tr></table>";
+        $lookup = new ImageService();
+        $photo_url = $lookup->lookupByEmail($email_address);
+    }
+
+    if (!$photo_url) {
+        echo "<table align=center><td>";
+        echo "We were unable to find any photos to display for $email_address. Please verify the email address and resubmit";
+        echo "</td></table>";
+    } else {
+        echo "<table align=center><tr><td align=center><img src=$photo_url></td></tr></table>";
+        echo "<table align=center><tr><td align=center>$email_address</td></tr></table>";
     }
 
     // do some data sanitiing.
@@ -42,4 +50,6 @@
     }
 
 ?>
+
+
 
